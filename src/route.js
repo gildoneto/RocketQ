4027dbc60953fc4735464b7960b1b9d3c5916a32
Gildo Neto <gildorama@gmail.com>
2021-06-25 16:17:36 -0300

    configura dataset id das perguntas

--- src/route.js
+++ src/route.js
@@ -6,4 +6,7 @@ route.get('/', (req, res) => res.render('index'))
 route.get('/room', (req, res) => res.render('room'))
 route.get('/create-pass', (req, res) => res.render('create-pass'))
 
+// como o form do modal tem que passar a info
+// route.post('/room/:room/:question/:action', (req, res) => res.render())
+
 module.exports = route
