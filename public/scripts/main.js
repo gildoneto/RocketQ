14dc5915f6c7a573cda52a7cafb72f537ce8446b
Gildo Neto <gildorama@gmail.com>
2021-06-25 13:03:01 -0300

    refatora modal

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,20 +2,29 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+
 //pegar todos os botoes que pertencem a classe check
 const checkButtons = document.querySelectorAll('.actions a.check')
 
 checkButtons.forEach(button => {
     //adicionar a escuta
-    button.addEventListener('click', event => {
-        modal.open()
-    })
+    button.addEventListener('click', handleClick)
 })
 
 const deleteButton = document.querySelectorAll('.actions a.delete')
 
 deleteButton.forEach(button => {
-    button.addEventListener('click', event => {
-        modal.open()
-    })
+    button.addEventListener('click', (event) => handleClick(event, false))
 })
+
+function handleClick(event, check = true){
+    const text = check ? 'Marcar como lida' : 'Excluir'
+    modalTitle.innerHTML = `${text} esta pergunta`
+    modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modal.open()
+}
