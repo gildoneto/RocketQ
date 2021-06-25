3f6a04bfa84e9a5b1b915e6e131a6539849ced8b
Gildo Neto <gildorama@gmail.com>
2021-06-25 14:34:35 -0300

    muda texto do modal, muda cor do botao

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -26,5 +26,7 @@ function handleClick(event, check = true){
     const text = check ? 'Marcar como lida' : 'Excluir'
     modalTitle.innerHTML = `${text} esta pergunta`
     modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+    check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
     modal.open()
 }
