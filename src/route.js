a9c42fd9da51f5a83d215431c49b719425be04ff
Gildo Neto <gildorama@gmail.com>
2021-06-25 17:15:57 -0300

    componentiza enter-room e create-pass

--- src/route.js
+++ src/route.js
@@ -3,10 +3,10 @@ const QuestionController = require('./controllers/QuestionController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render('index'))
-route.get('/room', (req, res) => res.render('room'))
-route.get('/create-pass', (req, res) => res.render('create-pass'))
+route.get('/', (req, res) => res.render('index', {page: 'enter-room'}))
+route.get('/create-pass', (req, res) => res.render('index', {page: 'create-pass'}))
 
+route.get('/room', (req, res) => res.render('room'))
 // como o form do modal tem que passar a info
 route.post('/room/:room/:question/:action', QuestionController.index)
 
