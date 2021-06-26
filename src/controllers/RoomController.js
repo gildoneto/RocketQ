66d0ddeb42a2b64db72fce4135a7a36bcc4364f9
Gildo Neto <gildorama@gmail.com>
2021-06-26 14:16:47 -0300

    nao permite criacao de sala em duplicidade

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,20 +5,31 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId
-        
-        for(var i = 0; i < 6; i++){
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-            roomId += Math.floor(Math.random() * 10).toString()
+        let isRoom = true
+
+        while(isRoom){
+            /* GERA O NUMERO DA SALA */
+            for(var i = 0; i < 6; i++){
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+
+            /* VERIFICA SE O NOMERO JA EXISTE */
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+
+            if(!isRoom){
+                /* PERSISTE NUMERO DA SALA E SENHA NO BANCO */
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                )  VALUES(
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+            }
         }
 
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        )  VALUES(
-            ${parseInt(roomId)},
-            ${pass}
-        )
-        `)
 
         db.close()
 
