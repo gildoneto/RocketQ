666dd9e673d4159a0d19fc77f6e66c3a557d7a1a
Gildo Neto <gildorama@gmail.com>
2021-06-26 17:25:59 -0300

    persiste pergunta com numero da sala

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,4 +1,7 @@
+const Database = require('../db/config')
+
 module.exports = {
+
     index(req, res){
         const roomId = req.params.room
         const questionId = req.params.question
@@ -11,5 +14,23 @@ module.exports = {
         action = ${action},
         password = ${password}
         `)
+    },
+
+    async create(req, res){
+        const db = await Database()
+        const question = req.body.question
+        const roomId = req.params.room
+
+        await db.run(`INSERT INTO questions(
+            title,
+            room,
+            read
+        )VALUES(
+            "${question}",
+            ${roomId},
+            0
+        )`)
+
+        res.redirect(`/room/${roomId}`)
     }
 }
