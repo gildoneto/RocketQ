4657e25fd003d9d201e7a40708b01e5c01e5993c
Gildo Neto <gildorama@gmail.com>
2021-06-27 11:36:01 -0300

    altera mensagem do console ao subir o servidor

--- src/server.js
+++ src/server.js
@@ -14,4 +14,4 @@ server.use(express.urlencoded({extended: true}))
 
 server.use(route)
 
-server.listen(3000, () => console.log('RODAAAAANDO'))
+server.listen(3000, () => console.log('SERVIDOR RODANDO!!!'))
