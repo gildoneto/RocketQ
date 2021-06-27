700fb48d661d721616ad4de5dca6a74e207e74ae
Gildo Neto <gildorama@gmail.com>
2021-06-27 18:11:29 -0300

    atualiza

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,8 +1,11 @@
 export default function Modal(){
-
+    // guarda todos os elementos dentro da classe '.modal-wrapper'
     const modalWrapper = document.querySelector('.modal-wrapper')
+    // guarda todos os elementos dentro que tenham ao mesmo tempo
+    // a classe '.button' e a '.cancel'
     const cancelButton = document.querySelector('.button.cancel')
 
+    //Adiciona um Event Listener de click, que ao ser acionada executa a funcao close
     cancelButton.addEventListener('click', close)
 
     function open(){
