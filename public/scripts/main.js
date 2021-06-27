1d7f3bb8cf99dd3ca3685c922d8c6841f2b65a3a
Gildo Neto <gildorama@gmail.com>
2021-06-27 18:11:09 -0300

    atualiza comments

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,26 +2,36 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
-
+//atribui a constante a tag <h2> da classe .modal
 const modalTitle = document.querySelector('.modal h2')
+
+//atribui a constante a tag <a> da classe .modal
 const modalDescription = document.querySelector('.modal p')
+
+//atribui a constante a tag <button> da classe .modal
 const modalButton = document.querySelector('.modal button')
 
 
 //pegar todos os botoes que pertencem a classe check
 const checkButtons = document.querySelectorAll('.actions a.check')
 
+//percorre todos os botoes check
 checkButtons.forEach(button => {
-    //adicionar a escuta
+    //adiciona um listener de click a cada um dos botoes
     button.addEventListener('click', handleClick)
 })
 
+// atribui a constante todos as tags <a> dentro da classe .delete,
+// que tambem estejam dentro da classe .actions
 const deleteButton = document.querySelectorAll('.actions a.delete')
 
+//percorre todos os botoes de delete da pagina
 deleteButton.forEach(button => {
+    //adiciona um listener de click a cada um dos botoes
     button.addEventListener('click', (event) => handleClick(event, false))
 })
 
+//funcao recebe um event e atribue o valor true a variavel check
 function handleClick(event, check = true){
     event.preventDefault()
     const text = check ? 'Marcar como lida' : 'Excluir'
@@ -36,5 +46,8 @@ function handleClick(event, check = true){
     modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
     modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
     check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
+    
+    /* CHAMA A FUNCAO open() DO modal.js
+    que adiciona a classe "active" na modal */
     modal.open()
 }
